94204c385ec7e4ed668e22d477f5114c29e0b518
michael seaman <mseaman26@gmail.com>
2022-11-09 11:43:44 -0800

    added skeleton homepage route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,24 @@
 const router = require('express').Router();
 const { User, Dogs } = require('../models');
 
-router.get('/login', (req, res) => {
+router.get('/', async (req, res) => {
+    try {
+        res.render('homepage', {
+            loggedIn: req.session.loggedIn,
+        })
+    }catch(err){
+        console.log(err);
+        res.status(500).json(err)
+    }
+})
+
+router.get('/login', async (req, res) => {
+    try{
+        res.render('login')
+    }catch(err){
+        console.log(err)
+        res.status(500).json(err)
+    }
     res.render('login')
 })
+module.exports = router
