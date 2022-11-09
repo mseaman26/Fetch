a6c73165c0961202617152e3268837c092211eef
michael seaman <mseaman26@gmail.com>
2022-11-09 12:51:44 -0800

    login js file, login form handler, not working yet

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,25 @@
 const router = require("express").Router();
 
-
+//log in
+router.post('/login', async (req, res) => {
+    try{
+        const currentUser = await User.findOne({
+            where: {
+                name: req.body.name
+            },
+        }) 
+        if (!currentUser){
+            res.status(404).json("Login Failed.  Incorrect username and/or password")
+            return
+        }
+        req.session.save(() => {
+            req.session.user_id = currentUser.id
+            req.session.loggedIn = true
+            res.status(200).json({message: "Login Successful!"})
+        })
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router;
---
+++ public/js/login.js
@@ -0,0 +1,24 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+    console.log("log in button")
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (username && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to log in.');
+      }
+    }
+  };
+
+  document
+  .querySelector('.custom-form-login')
+  .addEventListener('submit', loginFormHandler);
