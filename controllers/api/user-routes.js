d28f3599f4922fc0bb4373f7d2fdd09202a1735b
michael seaman <mseaman26@gmail.com>
2022-11-09 13:26:40 -0800

    pushing what I have so far for login and signup

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -6,7 +6,7 @@ router.post('/login', async (req, res) => {
     try{
         const currentUser = await User.findOne({
             where: {
-                name: req.body.name
+                email: req.body.email
             },
         }) 
         if (!currentUser){
