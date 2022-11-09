047894b877b8cf2a31f23ee7d8c76ed89e60b9b1
Daniel Packer <danielapacker@gmail.com>
2022-11-09 12:29:01 -0700

    finish dog routes

--- controllers/api/dog-routes.js
+++ controllers/api/dog-routes.js
@@ -1,41 +1,45 @@
-const router = require('express').Router();
-const { Dog, User } = require('../../models');
+const router = require("express").Router();
+const { Dog, User } = require("../../models");
 
-router.get('/', async (req, res) => {
-    // find all dogs
-    try {
-        const dogData = await Dog.findAll({
-            // add associations here
-        });
-        console.log(dogData);
-        res.status(200).json(dogData);
-        console.log()
-    } catch (err) {
-        res.status(500).json(err);
-    }
+router.get("/", async (req, res) => {
+  // find all dogs
+  try {
+    const dogData = await Dog.findAll({
+      // add associations here
+    });
+    console.log(dogData);
+    res.status(200).json(dogData);
+    console.log();
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', async (req, res) => {
-    // find dog by id
-    try {
-        const dogData = await Dog.findByPk(req.params.id, {
-            // add associations here
-        });
-        console.log(dogData);
-        if (!dogData) {
-            res.status(404).json({message: "No dog with this id"});
+router.get("/:id", async (req, res) => {
+  // find dog by id
+  try {
+    const dogData = await Dog.findByPk(req.params.id, {
+      // add associations here
+    });
+    console.log(dogData);
+    if (!dogData) {
+      res.status(404).json({ message: "No dog with this id" });
       return;
     }
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
-    }
+  }
 });
 
-router.get('/:id', async (req, res) => {
-    try {
+router.post("/:winner", async (req, res) => {
+  // winner is selected by user
+  try {
+  } catch (err) {}
+});
 
-    } catch (err) {
-
-    }
+router.post("/:loser", async (req, res) => {
+  // loser is determined by user
+  try {
+  } catch (err) {}
 });
