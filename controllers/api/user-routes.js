f438df5efb5989a6c548753e45e51357bea2d1d3
Daniel Packer <danielapacker@gmail.com>
2022-11-09 13:47:14 -0700

    all user route and user id route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,42 +1,37 @@
 const router = require("express").Router();
-const { User, Dog } = require("../../models");
+const { User, Dogs } = require("../../models");
 
 router.get("/", async (req, res) => {
-    // find all users
-    try {
-      const userData = await User.findAll({
-        // add associations here
-      });
-      console.log(userData);
-      res.status(200).json(userData);
-      console.log();
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+  // find all users
+  try {
+    const userData = await User.findAll({
+      // add associations here
+      include: [{ model: Dogs }],
+    });
+    console.log(userData);
+    res.status(200).json(userData);
+    console.log();
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.get("/:id", async (req, res) => {
-    // find user by id
-    try {
-      const userData = await User.findByPk(req.params.id, {
-        // add associations here
-      });
-      console.log(userData);
-      if (!userData) {
-        res.status(404).json({ message: "No user with this id" });
-        return;
-      }
-      res.status(200).json(userData);
-    } catch (err) {
-      res.status(500).json(err);
+  // find user by id
+  try {
+    const userData = await User.findByPk(req.params.id, {
+      // add associations here
+      include: [{ model: Dogs }],
+    });
+    console.log(userData);
+    if (!userData) {
+      res.status(404).json({ message: "No user with this id" });
+      return;
     }
-  });
-
-router.post("/:winner", async (req, res) => {
-    // winner is selected by user
-    try {
-    } catch (err) {}
-  });
-
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
