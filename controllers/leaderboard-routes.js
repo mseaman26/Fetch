b3a79d3adfef3895f6e292f4e3dbbafb4d637c5b
Daniel Packer <danielapacker@gmail.com>
2022-11-10 13:06:22 -0700

    add get for leaderboard routes

--- controllers/leaderboard-routes.js
+++ controllers/leaderboard-routes.js
@@ -1,4 +1,16 @@
 const router = require("express").Router();
+const { User, Dogs } = require("../models");
 
+router.get("/", async (req, res) => {
+  try {
+    const leaderboardData = await Dogs.findByPk(req.params.rating, {
+      include: [{ model: User }],
+    });
+    res.status(200).json(leaderboardData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
