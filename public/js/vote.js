d515cfe96477f60a8b449192a285bc6f7bc5042a
Philip Huang <philiphuang2@gmail.com>
2022-11-12 12:33:02 -0800

    commented out errors in vote.js

--- public/js/vote.js
+++ public/js/vote.js
@@ -1,10 +1,10 @@
 const heartClick = async (event) => {
   event.preventDefault();
   console.log("favorite button");
-  fetch('/api/dogs/vote',{
-    method: 'POST',
-    body: JSON.stringify({winner: ,loser:})
-  })
+  // fetch('/api/dogs/vote',{
+  //   method: 'POST',
+  //   body: JSON.stringify({winner: ,loser:})
+  // })
 };
 
 document
